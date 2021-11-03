1bbdd2e7fbe4a7dbb1abe1caab23e0cd0bc6d151
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-11-03 15:15:34 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Example from "./Example"
+import "./index.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Example />)
